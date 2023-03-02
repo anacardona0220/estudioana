25d35e87207296cf87bf308305f05a8711505c8e
anacardona0220 <anamariacb0220@gmail.com>
2023-03-02 10:20:51 -0500

    counter for and while

---
+++ CursoC/contadorfor.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+int k,n,j = 0;
+printf("\ningrese el valor de n :");
+scanf("%d", &n);
+for ( k = 0; k <= n; k++)
+{
+printf("%d\n",j);    /* code */
+j ++;
+}
+
+return 0;
+
+}
---
+++ CursoC/contadorwhile.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main ()
+{
+    int k = 0, n, j = 0;
+    printf("infrese el valor de n : ");
+    scanf("%d,", &n);
+    while (k < n)
+    {
+    k++;
+    j++;
+    }
+    printf("%d\n", j);
+return 0;
+    
+}
