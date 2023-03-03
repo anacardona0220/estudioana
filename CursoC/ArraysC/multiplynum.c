83d0eec05d5a0f5178512803c7660189d3e4f384
anacardona0220 <anamariacb0220@gmail.com>
2023-03-03 18:12:57 -0500

    tarajandoenC

---
+++ CursoC/ArraysC/multiplynum.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main ()
+{
+    int num [3] = {1,2,3};
+    int i;
+    int mul;
+    for (i = 0; i < 3; i++)
+    {  
+       mul = num[i] * 2;
+       printf("%d\n", mul);
+    }
+    
+    
+
+
+
+
+
+    return 0;
+}
